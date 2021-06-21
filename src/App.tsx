95f51a29c0276d88bd9ecf1782fc7c936a0e8f81
janucewicz11 <janucewicz11@gmail.com>
2021-06-22 01:22:35 +0200

    config

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./styles.scss";
+import IMAGE from "./i4csypoqpnb61.png";
+import SVG_IMAGE from './laptop-cms.svg';
+
+const App = () => {
+	const num = 0;
+	const num1 = 0;
+	return (
+		<div>
+			<h1>React TypeScript project my code</h1>
+			<img src={IMAGE} alt="tets" width='350'/>
+            <img src={SVG_IMAGE} alt="svg" width='350'/>
+		</div>
+	);
+};
+
+export default App;

0dd75a3ab6fa403461ffd963c9ba2b58ec7768a1
janucewicz11 <janucewicz11@gmail.com>
2021-06-22 20:47:42 +0200

    webpack config

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import "./styles.scss";
-import IMAGE from "./i4csypoqpnb61.png";
+import React from 'react';
+import './styles.scss';
 import SVG_IMAGE from './laptop-cms.svg';
 
-const App = () => {
-	const num = 0;
-	const num1 = 0;
-	return (
-		<div>
-			<h1>React TypeScript project my code</h1>
-			<img src={IMAGE} alt="tets" width='350'/>
-            <img src={SVG_IMAGE} alt="svg" width='350'/>
-		</div>
-	);
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>React TypeScript project my code</h1>
+      <img src={SVG_IMAGE} alt='svg' width='350' />
+    </div>
+  );
 };
 
 export default App;
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,10 +1,11 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
@@ -13,7 +14,7 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "babel-loader",
+            loader: 'babel-loader',
           },
         ],
       },
@@ -21,34 +22,37 @@ module.exports = {
         test: /\.(css|scss)$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
           },
         ],
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
-        {
-          test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-          type: 'asset/inline',
-        },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
     ],
   },
   output: {
-    path: path.resolve(__dirname, "..", "./build"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "..", "./src/index.html"),
+      template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'source', to: 'dest', noErrorOnMissing: true }],
     }),
   ],
-  //   stats: 'errors-only',
+  stats: 'errors-only',
 };
